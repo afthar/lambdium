0fe88d15bcaa99aeecf0deb46e4c252a93bf7705
Daniel Alter - 971692 <d.alter@uq.edu.au>
2018-06-26 17:01:12 +1000

    reading env var

--- examples/enrol.js
+++ examples/enrol.js
@@ -4,6 +4,10 @@
 // $browser = webdriver session
 // $driver = driver libraries
 // console.log will output to AWS Lambda logs (via Cloudwatch)
+console.log('Reading variables...');
+
+var uqc_student_id   = process.env.UQC_STUDENT_ID;
+console.log("Student id is: " + uqc_student_id);
 
 console.log('About to visit blackboard...');
 $browser.get('https://staging-blackboard.elearning.uq.edu.au/webapps/login/?action=default_login');
@@ -12,26 +16,26 @@ $browser.get('https://staging-blackboard.elearning.uq.edu.au/webapps/login/?acti
 $browser.findElement($driver.By.name('user_id')).sendKeys('uqc_admin');
 $browser.findElement($driver.By.name('password')).sendKeys('NH8#UGcbUQ@CamLA');
 $browser.findElement($driver.By.name('login')).click();
-$browser.wait($driver.until.titleIs('Welcome, UQCollege – Blackboard Learn'), 10000);
-$browser.getTitle().then(function(title) {
-    console.log("title is: " + title);
-    console.log('Finished running script!');
-});
-
-//_choose_course
-
-$browser.findElement($driver.By.partialLinkText('AUTOMATION_TESTING')).click();
-
-
-//_navigate
-$browser.findElement($driver.By.id('controlpanel.users.and.groups_groupExpanderLink')).click();
-$browser.findElement($driver.By.id('menuPuller')).click();
-
-$browser.findElement($driver.By.xpath('//*[@id="controlpanel.users.and.groups_groupContents"]/li[3]/a')).click();
-//enroll
-$browser.findElement($driver.By.css('#nav > li > a')).click();
-$browser.findElement($driver.By.id('userName')).sendKeys('100000009');
-$browser.findElement($driver.By.name('bottom_Submit')).click();
+// $browser.wait($driver.until.titleIs('Welcome, UQCollege – Blackboard Learn'), 10000);
+// $browser.getTitle().then(function(title) {
+//     console.log("title is: " + title);
+//     console.log('Finished running script!');
+// });
+//
+// //_choose_course
+//
+// $browser.findElement($driver.By.partialLinkText('AUTOMATION_TESTING')).click();
+//
+//
+// //_navigate
+// $browser.findElement($driver.By.id('controlpanel.users.and.groups_groupExpanderLink')).click();
+// $browser.findElement($driver.By.id('menuPuller')).click();
+//
+// $browser.findElement($driver.By.xpath('//*[@id="controlpanel.users.and.groups_groupContents"]/li[3]/a')).click();
+// //enroll
+// $browser.findElement($driver.By.css('#nav > li > a')).click();
+// $browser.findElement($driver.By.id('userName')).sendKeys('100000009');
+// $browser.findElement($driver.By.name('bottom_Submit')).click();
     // driver.implicitly_wait(2)
     // try:
     //     status = driver.find_element_by_id('goodMsg1').text.split(':', 1)[0]
